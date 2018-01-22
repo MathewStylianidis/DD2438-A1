b77279840b44012e5d240936450e9f9881d15d3f
MattSt <mattstyl@outlook.com>
2018-01-22 12:53:45 +0100

    .

---
+++ A1/Intermediate/Build/Win64/UE4Editor/Inc/A1/A1GameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef A1_A1GameModeBase_generated_h
+#error "A1GameModeBase.generated.h already included, missing '#pragma once' in A1GameModeBase.h"
+#endif
+#define A1_A1GameModeBase_generated_h
+
+#define A1_Source_A1_A1GameModeBase_h_15_RPC_WRAPPERS
+#define A1_Source_A1_A1GameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define A1_Source_A1_A1GameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAA1GameModeBase(); \
+	friend A1_API class UClass* Z_Construct_UClass_AA1GameModeBase(); \
+public: \
+	DECLARE_CLASS(AA1GameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/A1"), NO_API) \
+	DECLARE_SERIALIZER(AA1GameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define A1_Source_A1_A1GameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAA1GameModeBase(); \
+	friend A1_API class UClass* Z_Construct_UClass_AA1GameModeBase(); \
+public: \
+	DECLARE_CLASS(AA1GameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/A1"), NO_API) \
+	DECLARE_SERIALIZER(AA1GameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define A1_Source_A1_A1GameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AA1GameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AA1GameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AA1GameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AA1GameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AA1GameModeBase(AA1GameModeBase&&); \
+	NO_API AA1GameModeBase(const AA1GameModeBase&); \
+public:
+
+
+#define A1_Source_A1_A1GameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AA1GameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AA1GameModeBase(AA1GameModeBase&&); \
+	NO_API AA1GameModeBase(const AA1GameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AA1GameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AA1GameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AA1GameModeBase)
+
+
+#define A1_Source_A1_A1GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define A1_Source_A1_A1GameModeBase_h_12_PROLOG
+#define A1_Source_A1_A1GameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	A1_Source_A1_A1GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	A1_Source_A1_A1GameModeBase_h_15_RPC_WRAPPERS \
+	A1_Source_A1_A1GameModeBase_h_15_INCLASS \
+	A1_Source_A1_A1GameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define A1_Source_A1_A1GameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	A1_Source_A1_A1GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	A1_Source_A1_A1GameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	A1_Source_A1_A1GameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	A1_Source_A1_A1GameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID A1_Source_A1_A1GameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
